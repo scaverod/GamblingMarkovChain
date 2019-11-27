a34663f1f3106a4b19e7834685e804bac361a78d
Sergio <scaverod@gmail.com>
2019-11-27 14:31:29 +0100

    equal probability to bet

--- test.py
+++ test.py
@@ -14,6 +14,8 @@ bets = [(1, float(18 / 37)),
         (35, float(1 / 37))]
 
 p_bets = np.array([0.5, 0.2, 0.1, 0.1, 0.05, 0.025, 0.025])
+p_bets_eq = [float(1/len(bets))]*len(bets)
+print(sum(p_bets_eq))
 
 tmat = tm.create_transition_matrix(LEAVE_WITH_MIN, LEAVE_WITH_MAX, bets, p_bets)
 
